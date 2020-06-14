ffd592f329694714f13906db14a3451a4b65dd07
Omar Villa <o.villanueva@thearcane.com>
2020-06-14 03:19:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+
+def getRow(row)
+  return  "#{ row[0] }|#{ row[1] }|#{ row[2] }"
+end
+
+def display_board(board)
+  puts getRow(board[0,2])
+  puts"-----------"
+  puts getRow(board[3,5])
+  puts"-----------"
+  puts getRow(board[6,8])
+end

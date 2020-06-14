5dc0c24540b4bccb38c981b5fb1cd22a2dffb6ab
Omar Villa <o.villanueva@thearcane.com>
2020-06-14 03:37:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,9 +8,9 @@ end
 
 def display_board(board = [])
   divider = "-----------"
-  puts getRow(board[0,2])
+  puts " #{row[0] || ' '} | #{row[1] || ' '} | #{row[2] || ' '} "
   puts divider
-  puts getRow(board[3,5])
+  puts " #{board[3] || ' '} | #{board[4] || ' '} | #{board[5] || ' '} "
   puts divider
-  puts getRow(board[6,8])
+  puts " #{board[6] || ' '} | #{board[7] || ' '} | #{board[8] || ' '} "
 end
